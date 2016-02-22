7551e8418dca616efe872409be342bbb77e825e4
Evan Bonertz <bonertz.unc@gmail.com>
2016-02-22 09:50:22 -0500

    Update JS functionality and variables

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -1,11 +1,40 @@
-$(document).ready(function() {
-    $('.card').click(function() {
-        $(this).toggleClass('flipped');
-        $(this).children().toggleClass('name');
-    });
-  });
 
-  
+
+
+$(document).ready(function() {
+  //   var click1, click2, fouls=8;
+  //   function isMatched(card1, card2) {
+  // //This needs to return tru or false to see if click1 and click2 match
+  // }
+
+// function checkFouls( ) {}
+
+
+// function timerInitialize () {}
+
+//Function for card flip animation
+    function animateFlip(card) {
+            $(card).toggleClass('flipped');
+            $(card).children().toggleClass('name');
+    }
+
+
+
+    $('.card').click(function( ) {
+        animateFlip(this);
+    });
+});
+
+    //     if (click1) {
+    //     click1= this.className;
+    //   }
+    //     else {
+    //      click2 = this.className;
+    //      if (isMatched(click1, click2)) {
+        //If true than change class to permanently open
+        //if false change class to Not Flipped and remove one fouls
+        // check foul number
+
 
 
 
@@ -16,10 +45,6 @@ $(document).ready(function() {
         $(this).toggleClass('flipped');
     })
 });
-
-
-
-
 
 
 /*
