6cdb7cb50c164cf68a64424f742b8cada1efe404
ebonertz <bonertz.unc@gmail.com>
2016-03-02 13:13:33 -0500

    Update JS

--- Blue-Blood/Assets/JS/index.js
+++ Blue-Blood/Assets/JS/index.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function() {
 
 // Shotclock 30 seconds countdown
@@ -6,56 +5,102 @@ $(document).ready(function() {
     var countDown = setInterval (function() {
         shotClock = shotClock - 1; //subtract one from variable
         $('#timer').text("Shot Clock : " + shotClock);
-        // console.log(shotClock);
-        if (shotClock < 1) { // once secondsUntilGame reaches zero...
-      clearInterval(countDown); //stop the countdown
+        if (shotClock < 1) {
+        alert("Shotclock Expired"); //Alert user that Shotclock has expired
+        clearInterval(countDown); //stop the countdown
      }
-    }, 1000); //this happens every second until it doesn't happen anymore
+    }, 1000);
   });
 
-//Working Function for card flip animation//
-    // function animateFlip(card) {
-    //         $(card).toggleClass('flipped');
-    //         $(card).children().toggleClass('name');
-    // }
-    // $('.card').click(function( ) {
-    //     animateFlip(this);
-    // });
-var fouls= 8;
-var flippedNames = [];
-var flippedCards =[];
+    var fouls= 8,
+    firstFlip=true,
+    secondFlip=false,
+    flippedNames = [],
+    flippedCards =[];
+    heartDivs = $(".fa-heart").toArray();
 
- $('.card').click(function() {     // when any card is clicked...
-         if ($(this).attr('class').includes('flipped')) { // IF the card already has class flipped
-         return false;   //do nothing
-     }
+
+ $('.card').click(function() {
+        if(firstFlip){
+            $(this).addClass('flipped');
+            $(this).children().removeClass('name');
+            flippedNames.push($(this).children().attr('class'));
+            flippedCards.push($(this));
+            firstFlip=false;
+        }
         else {
-        $(this).addClass('flipped'); //1. Flip the card
-        $(this).children().toggleClass('name');  //2. Display the name
-        flippedCards.push($(this));   //3. add THIS to flippedCards[]
-        flippedNames.push($(this).children().attr('class'));  //4. add THIS card's icon's class to flippedIcons[]
+            fouls--;
+            console.log('Lives remaining'+fouls);
 
-        if (flippedNames.length === 2){  // IF flippedIcons[].length === 2
-        if (flippedNames[0] === flippedNames [1]) {  //do the icons on the flipped cards match?
-          console.log("Match");  // Display Match in the console.
-         flippedCards = [];  // empty array flippedCards
-         flippedIcons = [];  // empty array flippedIcons
-            }
+            $(this).addClass('flipped');
+            $(this).children().removeClass('name');
+            flippedNames.push($(this).children().attr('class'));
+            flippedCards.push($(this));
 
-         else {
-          console.log("not a match");
-          fouls = fouls -1;
-          console.log(fouls);
-          $(flippedCards[0]).removeClass('flipped');  //removes cardFlip class
-          $(flippedCards[1]).removeClass('flipped'); //from both .card (s)
-          $(flippedCards[0].children().toggleClass('name'));
-          $(flippedCards[1].children().toggleClass('name'));
-          flippedCards = [];  // empty array flippedCards
-          flippedIcons = [];  // empty array flippedIcons
-            }
-          };
-      };
-  });
+            setTimeout(function() {
+                if(flippedNames[0] !== flippedNames[1]){
+                    $(flippedCards[0]).removeClass('flipped');
+                    $(flippedCards[0]).children().toggleClass('name');
+                    $(flippedCards[1]).removeClass('flipped');
+                    $(flippedCards[1]).children().toggleClass('name');
+                    if(fouls){
+                        heartDivs[fouls].remove();
+                    }
+                    if(fouls===0){
+                        alert('You Fouled Out!');
+                    }
+                }
+                flippedNames = [];
+                flippedCards = [];
+            }, 1000);
+
+            firstFlip = true;
+
+        }; //This closes the else statement
+    }); //This closes the click function
+
+
+
+
+        //Original Working Function for card flip animation//
+        // function animateFlip(card) {
+        //         $(card).toggleClass('flipped');
+        //         $(card).children().toggleClass('name');
+        // }
+        // $('.card').click(function( ) {
+        //     animateFlip(this);
+        // });
+
+        //  if ($(this).attr('class').includes('flipped')) { // IF the card already has class flipped
+        //      return false;   //do nothing
+        //  }
+        // else {
+        //     $(this).addClass('flipped'); //1. Flip the card
+        //     $(this).children().toggleClass('name');  //2. Display the name
+        //     flippedCards.push($(this));   //3. add THIS to flippedCards[]
+        //     flippedNames.push($(this).children().attr('class'));  //4. add THIS card's icon's class to flippedIcons[]
+        //
+        //     if (flippedNames.length === 2){  // IF flippedIcons[].length === 2
+        //         if (flippedNames[0] === flippedNames [1]) {  //do the icons on the flipped cards match?
+        //             console.log("Match");  // Display Match in the console.
+        //             flippedCards = [];  // empty array flippedCards
+        //             flippedIcons = [];  // empty array flippedIcons
+        //         }
+        //
+        // else {
+        //       console.log("not a match");
+        //       fouls = fouls -1;
+        //       console.log(fouls);
+        //       $(flippedCards[0]).removeClass('flipped');  //removes cardFlip class
+        //       $(flippedCards[1]).removeClass('flipped'); //from both .card (s)
+        //       $(flippedCards[0].children().toggleClass('name'));
+        //       $(flippedCards[1].children().toggleClass('name'));
+        //       flippedCards = [];  // empty array flippedCards
+        //       flippedIcons = [];  // empty array flippedIcons
+        // }
+    // };
+    // };
+
   //           setTimeout(function() {  //delay what's inside this thing
   //
   //           $(".fouls").children().last().remove();// remove a heart
